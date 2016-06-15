45455070076f6b7955a099c036e2f71f759af32b
andrijasvrkota <andrija.svrkota@gmail.com>
2016-06-15 23:54:33 +0200

    Implementirana funkcionalnost programa da na osnovu ulaznog stringa predvidi naredna 3 karaktera

--- CollectionsHomework/src/bigram/Bigram.java
+++ CollectionsHomework/src/bigram/Bigram.java
@@ -25,5 +25,29 @@ public class Bigram {
 			Map.Entry<String, Integer> entry = (Entry<String, Integer>) iter.next();
 			System.out.println(entry.getKey() + " " + entry.getValue());
 		}
+
+		String test = "ja";
+		System.out.print("Za string '" +test);
+		for (int i = 0; i < 3; i++) {
+			int maxFrekvencija = 0;
+			String bigramSaNajvecomFrekvencijom = "";
+
+			for (Map.Entry<String, Integer> e : map.entrySet()) {
+				if (e.getKey().startsWith(test.substring(test.length()-1))) {
+					if (maxFrekvencija < e.getValue()) {
+						bigramSaNajvecomFrekvencijom = e.getKey();
+						maxFrekvencija = e.getValue();
+					}
+				}
+			}
+
+			if (maxFrekvencija > 0) {
+				char narednoSlovo = bigramSaNajvecomFrekvencijom.charAt(1);
+				test = test + "" + narednoSlovo;
+			}
+
+		}
+
+		System.out.println("' predvidjeni string je: " +test);
 	}
 }
